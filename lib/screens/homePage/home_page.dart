2d9ea8d428cda53c0559ed407842810c96b31925
Warda Jawad <wardasobaih@gmail.com>
2021-12-13 23:22:34 +0200

    build my showcaseview to start as soon as my  UI built up

--- lib/screens/homePage/home_page.dart
+++ lib/screens/homePage/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:showcaseview/showcaseview.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,6 +10,20 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  GlobalKey _one = GlobalKey();
+  GlobalKey _two = GlobalKey();
+
+  @override
+  void initState() {
+    super.initState();
+    //Start showcase view after current widget frames are drawn.
+    WidgetsBinding.instance!.addPostFrameCallback(
+      (_) => ShowCaseWidget.of(context)!.startShowCase(
+        [_one, _two],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Directionality(
@@ -44,6 +59,19 @@ class _HomePageState extends State<HomePage> {
             child: ListView.builder(
                 itemCount: 3,
                 itemBuilder: (context, index) {
+                  if (index == 0) {
+                    return Showcase(
+                      key: _one,
+                      description: 'إسحب لحذف المهمة',
+                      child: const Card(
+                        child: ListTile(
+                          leading: Icon(Icons.access_alarm_outlined),
+                          title: Text('المهمة الأولى'),
+                          trailing: Icon(Icons.more_vert),
+                        ),
+                      ),
+                    );
+                  }
                   return const Card(
                     child: ListTile(
                       leading: Icon(Icons.access_alarm_outlined),
@@ -53,29 +81,38 @@ class _HomePageState extends State<HomePage> {
                   );
                 }),
           ),
-          Container(
-            width: 110,
-            height: 45,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              color: Colors.brown,
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                Icon(
-                  Icons.add,
-                  color: Colors.white,
-                ),
-                Center(
-                  child: Text(
-                    "أضف مهمة",
-                    style: TextStyle(
-                      color: Colors.white,
+          Showcase(
+            overlayPadding: const EdgeInsets.all(10),
+            key: _two,
+            title: 'مهمة جديدة ',
+            description: 'إضغط لإضافة مهمة جديدة',
+            showcaseBackgroundColor: Colors.pink,
+            textColor: Colors.white,
+            shapeBorder: const CircleBorder(),
+            child: Container(
+              width: 110,
+              height: 45,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.brown,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(
+                    Icons.add,
+                    color: Colors.white,
+                  ),
+                  Center(
+                    child: Text(
+                      "أضف مهمة",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ],
