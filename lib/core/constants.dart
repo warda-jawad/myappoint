835e8c0cc009c8e893faaa533a1cc049697b1665
Warda Jawad <wardasobaih@gmail.com>
2021-12-16 12:33:22 +0200

    add constants

---
+++ lib/core/constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+Color primaryColor = const Color(0xff906269);
+TextTheme textTheme = const TextTheme(
+  bodyText1: TextStyle(
+    fontSize: 30,
+    color: Colors.white,
+    fontWeight: FontWeight.bold,
+    decoration: TextDecoration.none,
+  ),
+  bodyText2: TextStyle(
+    fontSize: 20,
+    color: Colors.white,
+    fontWeight: FontWeight.normal,
+    decoration: TextDecoration.none,
+  ),
+);
