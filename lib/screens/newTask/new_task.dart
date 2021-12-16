547f27310b1837f24bdea3fc40c854ae6c79470a
Warda Jawad <wardasobaih@gmail.com>
2021-12-16 12:00:17 +0200

    add time picker feature to Task/Reminder

--- lib/screens/newTask/new_task.dart
+++ lib/screens/newTask/new_task.dart
@@ -1,8 +1,10 @@
 import 'dart:ui';
 
+import 'package:day_night_time_picker/lib/daynight_timepicker.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 
 class NewTask extends StatefulWidget {
@@ -13,6 +15,11 @@ class NewTask extends StatefulWidget {
 }
 
 class _NewTaskState extends State<NewTask> {
+  TextEditingController timeinput = TextEditingController();
+  TimeOfDay _time = TimeOfDay.now().replacing(minute: 30);
+
+  void onTimeChanged(TimeOfDay newTime) => setState(() => _time = newTime);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,14 +33,9 @@ class _NewTaskState extends State<NewTask> {
               children: [
                 Container(
                   margin: const EdgeInsets.only(top: 90, left: 110),
-                  child: const Text(
+                  child: Text(
                     " أضف مهمة جديدة !",
-                    style: TextStyle(
-                      fontSize: 30,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      decoration: TextDecoration.none,
-                    ),
+                    style: Theme.of(context).textTheme.bodyText1,
                   ),
                 ),
                 Container(
@@ -86,12 +88,55 @@ class _NewTaskState extends State<NewTask> {
                     ),
                   ),
                 ),
+                InkWell(
+                  onTap: () async {
+                    Navigator.of(context).push(
+                      showPicker(
+                        context: context,
+                        value: _time,
+                        onChange: onTimeChanged,
+                      ),
+                    );
+                  },
+                  child: Container(
+                    height: 60,
+                    width: MediaQuery.of(context).size.width,
+                    margin: const EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 25,
+                    ),
+                    decoration: BoxDecoration(
+                      color: const Color(0xffe3f0f4),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Container(
+                      margin: const EdgeInsets.all(20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            "تاريخ المهمة",
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.blueGrey,
+                            ),
+                          ),
+                          Text(_time.format(context),
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(
+                                  fontSize: 20, color: Colors.black)),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
                 Container(
+                  margin: EdgeInsets.only(top: 190),
                   width: 110,
                   height: 45,
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(20),
-                    color: const Color(0xff906269),
+                    color: Theme.of(context).primaryColor,
                   ),
                   child: const Center(
                     child: Text(
